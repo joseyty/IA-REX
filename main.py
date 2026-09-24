c84ec586968968e7558c865b5975e356727208be
Igor jose <167133923+joseyty@users.noreply.github.com>
2026-09-23 23:25:41 -0300

    correções

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import sys
 
-from PySide6.QtWidgets
-import QApplication
-
+from PySide6.QtWidgets import QApplication
 from dispositivos import dispositivos
 from usuarios import usuarios
 from voz import falar
 import comandos
-from personagem import criar_personagem_personagem
+from personagem import criar_personagem
 
 app = QApplication(sys.argv)
-personagem = criar_personagem(app)
+personagem = criar_personagem()
 
 id_dispositivo = input("Digite o ID do dispositivo: ")
 
--- personagem.py
+++ personagem.py
@@ -1,14 +1,30 @@
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QLabel
 from PySide6.QtGui import QPixmap
 
 
-
-
-
 def criar_personagem():
-personagem = QLabel()
+    personagem = QLabel()
 
-personagem.setPixmap(QPixmap("personagem/standby_kyara.png"))
-personagem.show()
-return personagem
+
+
+    personagem.setPixmap(QPixmap("personagem/standby_kyara.png"))
+
+
+    personagem.setPixmap(imagem)
+    personagem.resize(imagem.size())
+
+    personagem.setWindowFlags(
+        Qt.FramelessWindowHint |
+        Qt.WindowStaysOnTopHint |
+        Qt.Tool
+
+    )
+
+    personagem.setAttribute(Qt.WA_TranslucentBackground)
+
+    personagem.show()
+
+
+
+    return personagem
 
